e10777a9e3c32521714265e81994b7fde92ad3b3
Robbie Clutton <robert.clutton@gmail.com>
2010-09-08 08:05:51 +0100

    add user test

--- webtest/api_test.php
+++ webtest/api_test.php
@@ -5,5 +5,21 @@ class TestOfApi extends WebTestCase {
         $this->get('http://localhost:3000');
 		$this->assertResponse(200);
     }
+
+	function testCreateUser(){
+		// setup
+		$params = array(
+			'first' => 'Davy',
+			'last' => 'Jones',
+			'age' => '600'
+		);
+		// act
+		$this->post('http://localhost:3000/user', $params);
+		// assert
+		$this->get('http://localhost:3000/user/davy');
+		$this->assertResponse(200);
+		$this->assertText('davy Jones');
+	}
+
 }
 ?>
